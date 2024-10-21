12127183d264cc0a28ba7724b230ed3941b0817f
ArvidSilverlock <ArvidSilverlock@gmail.com>
2024-10-21 21:21:46 +0100

    correct the font path *again*

--- streamlit_app.py
+++ streamlit_app.py
@@ -200,7 +200,7 @@ def main():
 <style>
 @font-face {{
   font-family: 'Virtual Rave';
-  src: url('/static/Virtual+Rave.ttf') format('ttf');
+  src: url('/app/static/Virtual+Rave.ttf') format('ttf');
 }}
 </style>
 <canvas id="svgCanvas" width="{canvasSize}" height="{canvasSize}" style="border:1px solid #000000;"></canvas>
